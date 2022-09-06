0e648308f09556839c1c36b2caa0be9266389ceb
Eugene Yatsik <eugene.yatsik@yandex.ru>
2022-09-06 19:19:01 +0300

    Add autotest

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,9 +4,9 @@ fun main() {
 //    "Mastercard", "Maestro", "Visa", "Мир", "VK Pay"
     val cardType = "Mastercard"
     val sumInMonth = 430_000_00
-    val money = 143_000_00
+    val amountTransfer = 143_000_00
 
-    if (CommissionCalc.calcCommission(cardType, sumInMonth, money) != money)
-        println("Комиссия составлет: ${CommissionCalc.calcCommission(cardType, sumInMonth, money)} коп.")
+    if (CommissionCalc.calcCommission(cardType, sumInMonth, amountTransfer) != -1)
+        println("Комиссия составлет: ${CommissionCalc.calcCommission(cardType, sumInMonth, amountTransfer)} коп.")
     else println("Ошибка перевода!")
 }
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,37 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import ru.netology.CommissionCalc.Companion.calcCommission
+
+class MainKtTest {
+
+    @Test
+    fun calcCommissionTest() {
+
+//        "Mastercard" & "Maestro"
+        assertEquals(-1, calcCommission("Maestro", amountTransfer = 0))
+        assertEquals(0, calcCommission("Maestro", 150_000_00, 45_000_00))
+        assertEquals(890_00, calcCommission("Maestro", 150_000_00, 145_000_00))
+        assertEquals(-1, calcCommission("Mastercard", amountTransfer = 245_000_00))
+        assertEquals(-1, calcCommission("Maestro", 570_000_00, 45_000_00))
+
+//        "Visa" & "Мир"
+        assertEquals(-1, calcCommission("Visa", amountTransfer = 30_00))
+        assertEquals(337_50, calcCommission("Visa", 150_000_00, 45_000_00))
+        assertEquals(1_087_50, calcCommission("Visa", amountTransfer = 145_000_00))
+        assertEquals(-1, calcCommission("Visa", 570_000_00, 45_000_00))
+
+//        "VK Pay"
+        assertEquals(-1, calcCommission(amountTransfer = 0))
+        assertEquals(0, calcCommission("VK Pay", 20_000_00, 12_000_00))
+        assertEquals(-1, calcCommission(amountTransfer = 25_000_00))
+        assertEquals(-1, calcCommission(sumInMonth = 35_000_00, amountTransfer = 10_000_00))
+
+//        Другие карты
+        assertEquals(-1, calcCommission("Тинькофф", amountTransfer = 15_000_00))
+
+//        Отрицательный перевод или 0
+        assertEquals(-1, calcCommission(amountTransfer = -15_000_00))
+        assertEquals(-1, calcCommission(amountTransfer = 0))
+    }
+}
